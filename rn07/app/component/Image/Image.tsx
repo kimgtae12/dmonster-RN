dd0588a94f9092be3a1fa26fa091b15cc5e557cc
leeyeun <dpdms0109@naver.com>
2023-06-22 16:37:40 +0900

    2023-06-22 react native fast Image 예시 작성 및 리드미 작성 완료

--- rn07/app/component/Image/Image.tsx
+++ rn07/app/component/Image/Image.tsx
@@ -1,14 +1,26 @@
 import React, {useEffect} from 'react';
 import {BackHandler, View} from 'react-native';
 import AutoHeightImage from 'react-native-auto-height-image';
+import FastImage from 'react-native-fast-image';
 
 export const Image = () => {
   return (
     <View>
       {/* <AutoHeightImage width={100} source={image} /> */}
-      <AutoHeightImage
+      {/* 단순 이미지 일 경우 사용 */}
+      {/* <AutoHeightImage
         width={100}
         source={{uri: 'http://placehold.it/350x150'}}
+      /> */}
+      {/* api에서 리스트로 불러올때 사용 */}
+      <FastImage
+        style={{width: 200, height: 200}}
+        source={{
+          uri: 'https://unsplash.it/400/400?image=1',
+          headers: {Authorization: 'someAuthToken'},
+          priority: FastImage.priority.normal,
+        }}
+        resizeMode={FastImage.resizeMode.contain}
       />
     </View>
   );
